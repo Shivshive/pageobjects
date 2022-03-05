b3639d84b3d9d732fe4d7b7f084eb13dbdffe8c2
Shiv Pandya <shivshive04@yahoo.com>
2022-03-05 09:35:22 +0530

    update secure page one

--- secure.page.js
+++ secure.page.js
@@ -10,6 +10,7 @@ class SecurePage extends Page {
      * define selectors using getter methods
      */
     get flashAlert() {
+         console.log('may be flashalert');
         return $('#flash');
     }
 }
