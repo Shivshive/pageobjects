d678f27d56fc3aafeaf2e95e630fe89fbafdb8f3
Shiv Pandya <shivshive04@yahoo.com>
2022-03-27 11:11:22 +0530

    wait for element exists

--- page.js
+++ page.js
@@ -8,7 +8,7 @@ module.exports = class Page {
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
     open(path) {
-		
+	console.log('wait for element exists');	
         return browser.url(`https://the-internet.herokuapp.com/${path}`)
     }
 }
